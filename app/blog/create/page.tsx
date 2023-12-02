3ad214975e6b2e661d1e61bd15e7105b2ac38845
TomoyaOgashiwa <47682517+TomoyaOgashiwa@users.noreply.github.com>
2023-12-02 10:31:31 +0900

    【UI、API】新規記事投稿作成画面 (#16)
    * 🚧 技術記事UI＆API作成（細かいリファクタリング必要）
    
    * ✨ 選択したブログを見る画面追加（ファイルだけ用意）

--- app/blog/create/page.tsx
+++ app/blog/create/page.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 
+import prisma from "@/lib/prisma";
+
+import MarkdownEditor from "@/components/editor/markdown-editor";
+
 // 技術記事の新規作成
-const Page = () => {
-  return <div>Create Blog Page</div>;
+const Page = async () => {
+  const tagList = await prisma.tag.findMany();
+  return (
+    <div>
+      <MarkdownEditor tagList={tagList} />
+    </div>
+  );
 };
 
 export default Page;
